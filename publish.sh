615153da7e3fe69c5051d1455d6a2f0828fc77fd
lin-zh-cn <38306568+lin-zh-cn@users.noreply.github.com>
2018-12-13 11:17:05 +0800

    cloud

--- publish.sh
+++ publish.sh
@@ -69,6 +69,12 @@ run_job(){
 		nohup /usr/local/java/jdk1.8.0_191/bin/java -jar /website/$whichone/$jobname/$jobname.jar --server=127.0.0.1 --server.port=$portnum > /website/$whichone/$jobname/logs/nohup-out-$date_str.log 2>&1 &
 		sleep 120
 	;;
+	'cloud')
+		echo "6.准备启动/website/${whichone}/${jobname}/${jobname}.jar，端口${portnum}"
+		date_str=$(date +%Y%m%d-%H%M%S)
+		nohup /usr/local/java/jdk1.8.0_191/bin/java -jar /website/$whichone/$jobname/$jobname.jar --server=0.0.0.0 --server.port=$portnum --spring.profiles.active=$active > /website/$whichone/$jobname/logs/nohup-out-$date_str.log 2>&1 &
+		sleep 120
+	;;
 	'qc')
 		echo "6.准备启动/website/${whichone}/${jobname}/${jobname}.jar，端口${portnum}"
 		date_str=$(date +%Y%m%d-%H%M%S)
@@ -123,6 +129,14 @@ cms(){
 	check_job
 	success_info
 }
+# cloud注册中心
+cloud(){
+  file_manage
+	kill_job
+	run_job
+	check_job
+	success_info
+}
 # qc服务
 qc(){
   file_manage
@@ -146,6 +160,9 @@ job_start(){
 	case $jobname in
 	'cms')
 		cms
+	;;
+	'cloud')
+		cloud
 	;;
 	'qc')
 		qc
